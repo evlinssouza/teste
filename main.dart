a8d3c346994472c41b321f05a1d549d1445e3e01
evlinssouza <77853218+evlinssouza@users.noreply.github.com>
2021-01-24 17:53:31 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => (MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: formUsuario(),
+    );
+  }
+
+  formUsuario() {}
+}
